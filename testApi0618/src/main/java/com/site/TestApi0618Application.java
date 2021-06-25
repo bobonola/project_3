fb5a120bab63c6b5b8ef2b3eccea345b1583946d
bobonola <80930167+bobonola@users.noreply.github.com>
2021-06-25 15:42:16 +0900

    Add files via upload

---
+++ testApi0618/src/main/java/com/site/TestApi0618Application.java
@@ -0,0 +1,13 @@
+package com.site;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestApi0618Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestApi0618Application.class, args);
+	}
+
+}
