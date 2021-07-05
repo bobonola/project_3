032f8e0e1be8b940180466a57a344c2e24416a89
jinyoung0323 <38928381+jinyoung0323@users.noreply.github.com>
2021-07-05 17:53:24 +0900

    0705_2 김진영

--- src/main/java/com/site/mapper/CartMapper.java
+++ src/main/java/com/site/mapper/CartMapper.java
@@ -11,11 +11,15 @@ public interface CartMapper {
 
 	// 장바구니 리스트 호출
 	List<CartVo> selectCartList();
-	// 장바구니 삭제
-	void deleteCartDelete(int cart_no);
+	// 장바구니 한 개 삭제
+	void deleteCartDelete(String email, int cart_no);
 	// 장바구니 전체삭제
-	void deleteCartAllDelete();
+	void deleteCartAllDelete(String email);
 	// 장바구니 추가
 	void cartInsertDo(CartVo cartVo);
+	// 선택구매 페이지 호출
+	List<CartVo> selectPayList(String email, int cart_no);
+	// 전체구매 페이지 호출
+	List<CartVo> selectAllPayList(String email);
 
 }
