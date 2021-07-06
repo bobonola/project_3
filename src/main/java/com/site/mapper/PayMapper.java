9239a7b5b71e7b2a2b153fec9f2c30e4f03379d9
jinyoung0323 <38928381+jinyoung0323@users.noreply.github.com>
2021-07-06 12:36:32 +0900

    0706 김진영

--- src/main/java/com/site/mapper/PayMapper.java
+++ src/main/java/com/site/mapper/PayMapper.java
@@ -2,11 +2,7 @@ package com.site.mapper;
 
 import org.apache.ibatis.annotations.Mapper;
 
-import com.site.vo.CartVo;
-
 @Mapper
 public interface PayMapper {
-
-	void insertPayment( CartVo cartVo );
 
 }
